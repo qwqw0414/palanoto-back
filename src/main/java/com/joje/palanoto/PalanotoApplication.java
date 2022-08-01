4ee7897777a7975acbad827ad508942eb636099e
qwqw0414 <xzdi0414@gmail.com>
2022-08-01 22:58:38 +0900

    기본 설정

---
+++ src/main/java/com/joje/palanoto/PalanotoApplication.java
@@ -0,0 +1,17 @@
+package com.joje.palanoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableAspectJAutoProxy
+public class PalanotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalanotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joje/palanoto/PalanotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joje.palanoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalanotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
